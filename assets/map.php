058ca6e1da93aa70d015896c627b2e06b32c9085
Edvini-Code <121399785+Edvini-Code@users.noreply.github.com>
2023-04-26 13:11:26 +0200

    Add files via upload

---
+++ assets/map.php
@@ -0,0 +1,17 @@
+<section class='map'>
+         <h1>Map</h1>
+        <span class="map_location">
+                <p>Home</p>
+                <i class="fas fa-chevron-right"></i>
+                <p class="all_map_para">Map</p> 
+        </span>
+        <div class="show_map">
+            <h1>School Location</h1>
+            <div class='show_map_inner'>
+              <img src="images/mapUBT.png" style="border:0;" allowfullscreen="" loading="lazy"></img>
+            </div>
+            <a href="https://www.google.com/maps/dir//J5W3%2BV69+UBT+College,+Prishtina+10000/@42.6548675,21.1485239,1323m/data=!3m1!1e3!4m8!4m7!1m0!1m5!1m1!1s0x13549e8d5d607f25:0xa31dd05b21bd09de!2m2!1d21.153124!2d42.647155">Klikoni këtu për të udhëtuar deri në UBT</a>
+
+         </div>
+         <h2>UBT School System</h2>
+</section>
